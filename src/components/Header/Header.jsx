34d5b07d5c76e976d19370bc38ad66ccca74a97f
RobertKaniuth <robert.kaniuth@gmx.de>
2023-01-17 12:05:12 +0100

    added link colorchange

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,22 +2,33 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./header.module.css";
 
+const handleActiveLink = ({ isActive }) => {
+  return isActive ? { color: "red" } : null;
+};
 const Header = () => (
   <div>
     <header className={styles.header}>
       <i className="styles.logo">
-        <NavLink className={styles.link} to="/">
-          Harz5
+        <NavLink style={handleActiveLink} className={styles.link} to="/">
+          ❤❤❤❤❤
         </NavLink>
       </i>
       <nav className={styles.nav}>
-        <NavLink className={styles.link} to="/flashbook">
+        <NavLink
+          style={handleActiveLink}
+          className={styles.link}
+          to="/flashbook"
+        >
           Flashbook
         </NavLink>
-        <NavLink className={styles.link} to="/commission_work">
+        <NavLink
+          style={handleActiveLink}
+          className={styles.link}
+          to="/commission_work"
+        >
           Commission Work
         </NavLink>
-        <NavLink className={styles.link} to="/contact">
+        <NavLink style={handleActiveLink} className={styles.link} to="/contact">
           Contact
         </NavLink>
       </nav>
